e17856db4c94b1f3d93404a5563c31b781c2539d
Scottie Enriquez <scottie@enriquez.xxx>
2024-06-01 14:10:15 -0700

    Adding ingress

--- 00-eksctl-configuration/create-cluster.sh
+++ 00-eksctl-configuration/create-cluster.sh
@@ -1,4 +1,4 @@
 # before running these commands, first authenticate with AWS (e.g., aws configure sso)
 eksctl create cluster -f cluster.yaml
 # if connecting to an existing cluster
-eksctl utils write-kubeconfig --cluster=eksctl-cluster
+eksctl utils write-kubeconfig --cluster=learning-kubernetes
---
+++ 06-ingress/commands.sh
@@ -0,0 +1,35 @@
+# install AWS Load Balancer Controller
+# https://docs.aws.amazon.com/eks/latest/userguide/lbc-manifest.html
+curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.7.2/docs/install/iam_policy.json
+aws iam create-policy \
+    --policy-name AWSLoadBalancerControllerIAMPolicy \
+    --policy-document file://iam_policy.json
+rm iam_policy.json
+eksctl utils associate-iam-oidc-provider --region=us-west-2 --cluster=learning-kubernetes --approve
+eksctl create iamserviceaccount \
+  --cluster=learning-kubernetes \
+  --namespace=kube-system \
+  --name=aws-load-balancer-controller \
+  --role-name AmazonEKSLoadBalancerControllerRole \
+  --attach-policy-arn=arn:aws:iam::$AWS_ACCOUNT_ID:policy/AWSLoadBalancerControllerIAMPolicy \
+  --approve
+kubectl apply \
+    --validate=false \
+    -f https://github.com/jetstack/cert-manager/releases/download/v1.13.5/cert-manager.yaml
+curl -Lo v2_7_2_full.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.7.2/v2_7_2_full.yaml
+sed -i.bak -e '596,604d' ./v2_7_2_full.yaml
+sed -i.bak -e 's|your-cluster-name|learning-kubernetes|' ./v2_7_2_full.yaml
+kubectl apply -f v2_7_2_full.yaml
+rm v2_7_2_full.yaml* 
+kubectl get deployment -n kube-system aws-load-balancer-controller
+
+# apply maniftests
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f ingress.yaml
+# gets address (e.g, http://k8s-default-ingress-08daebdfec-204015293.us-west-2.elb.amazonaws.com/) that can be opened in a web browser
+kubectl describe ingress 
+# clean up
+kubectl delete -f ingress.yaml
+kubectl delete -f service.yaml
+kubectl delete -f deployment.yaml
