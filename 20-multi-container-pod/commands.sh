a3beffaff60137f7fde35cd1410feb550e067c64
Scottie Enriquez <scottie@enriquez.xxx>
2024-06-16 17:52:41 -0700

    Adding multi-container pod examples

---
+++ 20-multi-container-pod/commands.sh
@@ -0,0 +1,26 @@
+assumes cluster created from 00-eksctl-configuration first
+kubectl apply -f database-configmap.yaml
+kubectl apply -f backend-deployment.yaml
+
+# check that the primary container is not yet running because the init container has not completed
+# STATUS shows as Init:0/1
+kubectl get pods
+
+# deploy database and service
+kubectl apply -f database-deployment.yaml
+kubectl apply -f database-service.yaml
+
+# verify that init container has completed
+# get database pod
+kubectl get pods
+# forward ports
+kubectl port-forward pod/postgres-database-697695b774-xcp9p 9000:8080
+# open Adminer in browser
+# see screenshot for logging in
+wget http://localhost:9000
+
+# clean up
+kubectl delete -f database-configmap.yaml
+kubectl delete -f database-deployment.yaml
+kubectl delete -f database-service.yaml
+kubectl delete -f backend-deployment.yaml
